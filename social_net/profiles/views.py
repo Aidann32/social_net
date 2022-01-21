d563a7b315b4699d79f629d6a9086f1e501967f0
Aidann32 <46024391+Aidann32@users.noreply.github.com>
2022-01-22 00:44:20 +0500

    Добавление странички для приглашения и удаления друзей
    Также проведен небольшой рефакторинг кода

--- social_net/profiles/views.py
+++ social_net/profiles/views.py
@@ -2,6 +2,8 @@ from django.core.files.base import File
 from django.shortcuts import render
 from .models import Profile,Relationship
 from .forms import ProfileModelForm
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 def my_profile_view(request):
     user_obj=Profile.objects.get(user=request.user)
@@ -25,15 +27,43 @@ def invites_received_view(request):
     context={'qs':qs,}
     return render(request,'profiles/my_invites.html',context)
 
-def profile_list_view(request):
-    user=request.user
-    qs=Profile.objects.get_all_profiles(user)
-    context={'qs':qs,}
-    return render(request,'profiles/profiles_list.html',context)
-
 def invite_profile_list_view(request):
     user=request.user
     qs=Profile.objects.get_all_profiles_to_invite(user)
     context={'qs':qs,}
     return render(request,'profiles/to_invite_list.html',context)
 
+def profile_list_view(request):
+    user=request.user
+    qs=Profile.objects.get_all_profiles(user)
+    context={'qs':qs,}
+    return render(request,'profiles/profiles_list.html',context)
+
+class ProfileListView(ListView):
+    model=Profile
+    template_name='profiles/profiles_list.html'
+    context_object_name='qs'
+
+    def get_queryset(self):
+        qs=Profile.objects.get_all_profiles(self.request.user)
+        return qs
+
+    def get_context_data(self,*args,**kwargs):
+        context=super().get_context_data(**kwargs)
+        user=User.objects.get(username__iexact=self.request.user)
+        profile=Profile.objects.get(user=user)
+        context['profile']=profile
+        receiver=Relationship.objects.filter(sender=profile)
+        sender=Relationship.objects.filter(receiver=profile)
+        rel_receiver=[]
+        rel_sender=[]
+        for item in receiver:
+            rel_receiver.append(item.receiver.user)
+        for item in sender:
+            rel_sender.append(item.sender.user)
+        context['rel_receiver']=rel_receiver
+        context['rel_sender']=rel_sender
+        context["is_empty"]=False
+        if len(self.get_queryset())==0:
+            context['is_empty']=True
+        return context
